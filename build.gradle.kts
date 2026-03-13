2bd4bf8a574c8ad0e879c1c2bb8c0a42e9d2a1d3
RikaCelery <94585272+RikaCelery@users.noreply.github.com>
2026-03-13 10:55:54 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,31 +18,27 @@ application {
 
 dependencies {
     implementation("ch.qos.logback:logback-classic:1.5.13")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.1")
 
     implementation("io.ktor:ktor-server-core-jvm:3.4.1")
     implementation("io.ktor:ktor-server-cio-jvm:3.4.1")
     implementation("io.ktor:ktor-server-cors:3.4.1")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.4.1")
 
     implementation("io.ktor:ktor-client-core-jvm:3.4.1")
     implementation("io.ktor:ktor-client-okhttp-jvm:3.4.1")
     implementation("io.ktor:ktor-client-logging:3.4.1")
     implementation("io.ktor:ktor-client-websockets:3.4.1")
-    implementation("io.ktor:ktor-client-content-negotiation-jvm")
+    implementation("io.ktor:ktor-client-content-negotiation-jvm:3.4.1")
 
     implementation("io.github.nomisrev:kotlinx-serialization-jsonpath:1.0.0")
-    // https://mvnrepository.com/artifact/org.jsoup/jsoup
     implementation("org.jsoup:jsoup:1.18.1")
-    // https://mvnrepository.com/artifact/org.jline/jline
     implementation("org.jline:jline:3.30.6")
-    // https://mvnrepository.com/artifact/commons-cli/commons-cli
     implementation("commons-cli:commons-cli:1.11.0")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation(platform("org.junit:junit-bom:6.0.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("io.ktor:ktor-server-test-host-jvm")
 }
 
 tasks.test {
